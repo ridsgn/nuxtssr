cb00889cf408df11bc1ec17f4ce58bbcf57ec761
Rizky <rizkyfirmansyah967@gmail.com>
2020-07-15 16:31:49 +0700

    first commit

---
+++ components/TheNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <div>
+      <a href="">
+        Nuxt Fundamentals
+      </a>
+
+      <span>A Vue School Course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto px-4">
+    <article>
+      <h1 class="title">What is Balut ?</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur
+        est culpa illo reiciendis. Asperiores eius officiis amet nemo voluptatum
+        deserunt a quidem, officia exercitationem, adipisci voluptatem
+        reiciendis, aliquid fuga! Non.
+      </p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
